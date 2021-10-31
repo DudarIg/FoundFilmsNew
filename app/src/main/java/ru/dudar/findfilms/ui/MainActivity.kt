c58d6c3576eb1e88e5e32026c7dc3e53d64c6ebf
pakad <pakad@mail.ru>
2021-10-31 09:36:43 +0300

    Пустой проект

--- app/src/main/java/ru/dudar/findfilms/ui/MainActivity.kt
+++ app/src/main/java/ru/dudar/findfilms/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.dudar.findfilms
+package ru.dudar.findfilms.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dudar.findfilms.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
