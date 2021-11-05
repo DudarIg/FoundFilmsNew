8fdeb42c731448fe84c59f19adb31047b593efab
pakad <pakad@mail.ru>
2021-11-05 23:07:12 +0300

    Переделал разметки activity по ориентации

--- app/src/main/java/ru/dudar/findfilms/ui/MainActivity.kt
+++ app/src/main/java/ru/dudar/findfilms/ui/MainActivity.kt
@@ -1,14 +1,17 @@
 package ru.dudar.findfilms.ui
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ru.dudar.findfilms.R
 import ru.dudar.findfilms.data.FilmsViewModel
 import ru.dudar.findfilms.domain.MyAdapter
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,13 +20,22 @@ class MainActivity : AppCompatActivity() {
 
         val filmsViewModel = ViewModelProvider(this).get(FilmsViewModel::class.java)
 
-        val recyclerViewTop: RecyclerView = findViewById(R.id.recycler_top)
-        recyclerViewTop.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        recyclerViewTop.adapter = MyAdapter(filmsViewModel.films_top, this)
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            val recyclerViewTop: RecyclerView = findViewById(R.id.recycler_top)
+            recyclerViewTop.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            recyclerViewTop.adapter = MyAdapter(filmsViewModel.films_top, this)
 
-        val recyclerViewBottom: RecyclerView = findViewById(R.id.recycler_bottom)
-        recyclerViewBottom.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        recyclerViewBottom.adapter = MyAdapter(filmsViewModel.films_bottom, this)
+            val recyclerViewBottom: RecyclerView = findViewById(R.id.recycler_bottom)
+            recyclerViewBottom.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            recyclerViewBottom.adapter = MyAdapter(filmsViewModel.films_bottom, this)
+        }
+
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            val recyclerViewTop: RecyclerView = findViewById(R.id.recycler_top)
+            recyclerViewTop.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            recyclerViewTop.adapter = MyAdapter(filmsViewModel.films_top + filmsViewModel.films_bottom, this)
+
+        }
 
     }
 
