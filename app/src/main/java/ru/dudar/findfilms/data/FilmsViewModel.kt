bfec90c510b15b3277b792c820c79bc417375172
pakad <pakad@mail.ru>
2021-11-21 13:09:45 +0300

    удаление ненужных ресурсов

--- app/src/main/java/ru/dudar/findfilms/data/FilmsViewModel.kt
+++ app/src/main/java/ru/dudar/findfilms/data/FilmsViewModel.kt
@@ -5,41 +5,37 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.dudar.findfilms.R
 import ru.dudar.findfilms.domain.TMDBGenres
+import ru.dudar.findfilms.domain.films.Films
 import kotlin.random.Random
 
 class FilmsViewModel : ViewModel() {
-    val films_top = mutableListOf<Film>()
-    val films_bottom = mutableListOf<Film>()
+    var films_top = mutableListOf<Film>()
+    var films_bottom = mutableListOf<Film>()
+    var filmsJson: Films? = null
     private val films: TMDBGenres by lazy { RetrofitTMDBGenresImpl() }
-    private val films1: TMDBGenres by lazy { RetrofitTMDBGenresImpl() }
+
     init {
-
         Thread {
-            val films = films.getFilmsGanre(14)
+            filmsJson = films.getFilmsGanre(14)
+            films_top = filmsToList(filmsJson!!)
 
-            films!!.items.forEach {
-                val film = Film()
-                film.id = it.id
-                film.photo = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${it.posterPath}"
-                film.title = it.title
-                film.year = it.releaseDate.substring(0,4)
-                film.style = films.id
-                films_top.add(film)
-            }
-            val films1 = films1.getFilmsGanre(35)
+            filmsJson = films.getFilmsGanre(35)
+            films_bottom = filmsToList(filmsJson!!)
 
-            films1!!.items.forEach {
-                val film1 = Film()
-                film1.id = it.id
-                film1.photo = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${it.posterPath}"
-                film1.title = it.title
-                film1.year = it.releaseDate.substring(0,4)
-                film1.style = films1.id
-                films_bottom.add(film1)
-            }
-            //Thread.sleep(5_000)
         }.start()
+    }
 
-
+    fun filmsToList(jsonFilms: Films): MutableList<Film> {
+        val filmList = mutableListOf<Film>()
+        jsonFilms.items.forEach {
+            val film = Film()
+            film.id = it.id
+            film.photo = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${it.posterPath}"
+            film.title = it.title
+            film.year = it.releaseDate.substring(0, 4)
+            film.style = jsonFilms.id
+            filmList.add(film)
+        }
+        return filmList
     }
 }
