3cab05f7cb232b28cfcdbb51933eb234384922ff
pakad <pakad@mail.ru>
2021-11-28 09:10:15 +0300

    Начал Room

---
+++ app/src/main/java/ru/dudar/findfilms/db/FilmsView.kt
@@ -0,0 +1,16 @@
+package ru.dudar.findfilms.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "films")
+data class FilmsView(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "title") var title: String = "",
+    @ColumnInfo(name = "date") val date: Date = Date(),
+    @ColumnInfo(name = "view") var isViews: Boolean = false
+)
+
